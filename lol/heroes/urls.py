b0602ef7225b3e6f9db23df6da1070e32d1a462a
Ondřej Prokopius <117259061+OndrejProkopius@users.noreply.github.com>
2025-04-06 21:09:42 +0200

    Delete lol directory

--- lol/heroes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("hero/<int:hero_id>/", views.hero_detail, name="hero_detail"),
-    path("category/<int:category_id>/", views.category_detail, name="category_detail"),
-    path("author/<int:author_id>/", views.author_detail, name="author_detail"),
-]
--- lol/settings.py
+++
@@ -1,5 +0,0 @@
-python manage.py startapp heroes
-INSTALLED_APPS = [
-    ...
-    'heroes',
-]
